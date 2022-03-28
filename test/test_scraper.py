00bf08bf2ced07d4b36e7aac38124779106c5ef1
barnabymorgan <67784746+barnabymorgan@users.noreply.github.com>
2022-03-28 21:10:54 +0100

    update test_scraper

--- test/test_scraper.py
+++ test/test_scraper.py
@@ -1,10 +1,5 @@
+import time
 import unittest
-import urllib.request
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from scraper.furniture_scraper import FurnitureScraper
 
 
@@ -16,56 +11,20 @@ class FunitureScraperTestCase(unittest.TestCase):
         the __init__ of the test class
         """
         self.bot = FurnitureScraper(URL, category)
-        self.driver = (webdriver.Chrome(service=Service(
-            ChromeDriverManager().install())))
-
-        self.driver.get(self.bot)
-
+        time.sleep(5)
         return super().setUp()
-
-    # def test_get_furniture_data():
-    #    pass
-
-    # def test_make_data_store(self, folder: str) -> None:
-    #    pass
-
-    # def test_get_furniture_images(self, name: str) -> None:
-    #    pass
-
-    def test_land_first_page(self) -> None:
-        self.driver.get(self.homepage + self.category)
-        pass
-
-    def test_accept_cookies(self) -> None:
-        self.bot.accept_cookies()
-        pass
-
-    def test_x_out_sign_up(self) -> None:
-        self.bot.x_out_sign_up()
-        pass
-
-    def test_scroll(self, x=0, y=1000) -> None:
-        pass
-
-    def test_click_next_page(self) -> None:
-        pass
-
-    def test_click_search_bar(self) -> None:
-        pass
-
-    def test_write_data_to_file(self, folder, data) -> None:
-        pass
-
-    def test_write_images_to_file(self, image, name, number) -> None:
-        pass
 
     def test_generate_uuid(self) -> str:
         assert type(FurnitureScraper.generate_uuid(self)) is str
 
-    def test_upload_image_to_cloud() -> str:
-        """
-            check if we can see the bucket that we are uploading to 
-        """
+    def test_accept_cookies(self):
+        self.bot.x_out_sign_up("//div[@data-testid='om-overlays-close']")
+
+    def test_get_chair_price(self,
+                             str_price='£1,009'):
+        expected_price = 1009
+        actual_price = self.bot.get_chair_price(str_price)
+        self.assertEqual(expected_price, actual_price)
 
     def tearDown(self) -> None:
         """
