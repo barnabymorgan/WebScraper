3c8deaf987621722681589fe74a6f3f903a08dba
Barnaby Morgan <barnabymorgan@192.168.1.204>
2022-03-28 21:49:54 +0100

    README updates

--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 
 This project is a general purpose web scraper, with a specialisation towards scraping from http://swooneditions.com/
 
-Unittest
-  - It has unit tests 
-  - These test
+> - Basic Functionality is inherited from the Scraper class 
+> - The data storage is integrated with cloud services (S3, RDS)
+> - The project contains the unittest class, which is used to inspect the resulting data after the code is run.
 
-It uses AWS to store the data
-  - S3 bucket
-  - RDS 
+## Basic information
+
+Collect the statistics from http://swooneditions.com/ for the set furniture category.    
+> - Data: Name, type, description, category, images
+
+Also collects basic information and an associated image for each game.   
+> - Tools used : Python, Selenium, SQL,   
+> - Services used: AWS S3, RDS, EC2. Github, Dockerhub, Prometheus, Grafana   
--- test/test_scraper.py
+++ test/test_scraper.py
@@ -1,71 +1,30 @@
 import unittest
-import urllib.request
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from scraper.furniture_scraper import FurnitureScraper
+
+from zmq import NULL
+import scraper.furniture_scraper as fs
 
 
 class FunitureScraperTestCase(unittest.TestCase):
-    def setUp(self,
-              URL='https://www.swooneditions.com/',
-              category='chairs') -> None:
+    def setUp(self):
         """
         the __init__ of the test class
         """
-        self.bot = FurnitureScraper(URL, category)
-        self.driver = (webdriver.Chrome(service=Service(
-            ChromeDriverManager().install())))
-
-        self.driver.get(self.bot)
+        # self.bot = fs.FurnitureScraper('https://www.swooneditions.com/', 'chairs')
+        URL = 'https://www.swooneditions.com/'
+        category = 'chairs'
+        furinture_categories = ['chairs',
+                            'tables',
+                            'lighting']
+        self.furniture_scraper = fs.FurnitureScraper(URL, category)
+        self.furniture_scraper.go_to_furniture_category()
 
         return super().setUp()
 
-    # def test_get_furniture_data():
-    #    pass
-
-    # def test_make_data_store(self, folder: str) -> None:
-    #    pass
-
-    # def test_get_furniture_images(self, name: str) -> None:
-    #    pass
-
-    def test_land_first_page(self) -> None:
-        self.driver.get(self.homepage + self.category)
-        pass
-
-    def test_accept_cookies(self) -> None:
-        self.bot.accept_cookies()
-        pass
-
-    def test_x_out_sign_up(self) -> None:
-        self.bot.x_out_sign_up()
-        pass
-
-    def test_scroll(self, x=0, y=1000) -> None:
-        pass
-
-    def test_click_next_page(self) -> None:
-        pass
-
-    def test_click_search_bar(self) -> None:
-        pass
-
-    def test_write_data_to_file(self, folder, data) -> None:
-        pass
-
-    def test_write_images_to_file(self, image, name, number) -> None:
-        pass
-
     def test_generate_uuid(self) -> str:
-        assert type(FurnitureScraper.generate_uuid(self)) is str
+        assert type(fs.FurnitureScraper.generate_uuid(self)) is str
 
-    def test_upload_image_to_cloud() -> str:
-        """
-            check if we can see the bucket that we are uploading to 
-        """
+    def test_click_search_bar(self): 
+        assert type(fs.FurnitureScraper.click_search_bar(self)) is not NULL
 
     def tearDown(self) -> None:
         """
@@ -75,4 +34,4 @@ class FunitureScraperTestCase(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
